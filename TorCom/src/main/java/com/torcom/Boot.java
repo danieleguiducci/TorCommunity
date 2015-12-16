586e3ae55ffb173071c7010c481f1e466239d1a5
daniele.guiducci <daniele.guiducci@gmail.com>
2015-12-16 17:16:06 +0000

    Apache derby and liquidbase

--- TorCom/src/main/java/com/torcom/Boot.java
+++ TorCom/src/main/java/com/torcom/Boot.java
@@ -2,17 +2,30 @@ package com.torcom;
 
 import io.vertx.core.Vertx;
 import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-public class Boot {
+@EnableTransactionManagement
+public class Boot implements InitializingBean{
     @Value("${port}")
     private int port;
     @Autowired
     private ProxyServer proxyServer;
+    @Autowired
+    private DataSource dataSource;
+    @Autowired
+    private SpringLiquibase liquid;
     /**
      * @param args the command line arguments
      */
@@ -25,4 +38,20 @@ public class Boot {
        // Vertx.vertx().deployVerticle(staticServer);
        Vertx.vertx().deployVerticle(proxyServer);
     }
+    @Bean
+    @ConfigurationProperties("liquibase")
+    public SpringLiquibase liquibase() {
+        SpringLiquibase sl=new SpringLiquibase();
+        sl.setChangeLog("classpath:db-changes.xml");
+        sl.setDataSource(dataSource);
+        sl.setContexts("production");
+        return sl;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+    }
+
+
 }
